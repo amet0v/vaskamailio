6f8d7c9680506762d20147938878701ed3e05faf
Denis Ametov <dametov@in.nurtelecom.kg>
2026-01-15 10:45:05 +0600

    added reading dot_env

--- src/main/java/com/nurtel/vaskamailio/VaskamailioApplication.java
+++ src/main/java/com/nurtel/vaskamailio/VaskamailioApplication.java
@@ -4,12 +4,31 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class VaskamailioApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
+		String envPath = System.getProperty("ENV_FILE");
+		System.out.println("ENV_FILE = " + System.getProperty("ENV_FILE"));
+
+		Dotenv dotenv = (envPath != null && !envPath.isBlank())
+				? Dotenv.configure()
+				.directory(new File(envPath).getParent())
+				.filename(new File(envPath).getName())
+				.ignoreIfMissing()
+				.load()
+				: Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(e -> {
+			if (System.getProperty(e.getKey()) == null && System.getenv(e.getKey()) == null) {
+				System.setProperty(e.getKey(), e.getValue());
+			}
+		});
 
 		SpringApplication.run(VaskamailioApplication.class, args);
 	}
